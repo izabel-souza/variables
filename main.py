b9c214566e06cd2f2ce9985250cfc7f08cb1a9e8
izabel-souza <izabelsoaresbds@yahoo.com>
2022-04-02 16:07:14 -0300

    minor changes

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-# nome = "Izabel"
-# empresa = "VNW"
-# qtde_funcionarios = 120
-# mediaMensalidade = 1.250
-# print(nome + "trabalha na empresa " + empresa)
-# print("A média da mensalidade é " + str(mediaMensalidade))
-
 nome = input()
 empresa = input()
 qtde_funcionarios = int(input())
